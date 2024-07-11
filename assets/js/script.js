5f5c9f82ad90c3cd6e92b07bc2084040a2625279
Gabriel Souza <92094301+gabrielsl11@users.noreply.github.com>
2024-07-11 16:50:45 -0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,21 @@
 const value = document.getElementById('value');
-const plus_btn = document.getElementById('plus');
-const minus_btn = document.getElementById('minus');
-const reset_btn = document.getElementById('reset');
+const plus = document.getElementById('plus');
+const minus = document.getElementById('minus');
+const reset = document.getElementById('reset');
 
-const updateValue = () => {
-    value.innerHTML = count;
-}
+let updateValue = 0;
 
-let count = 0;
-let intervalId = 0;
-
-plus_btn.addEventListener('mousedown', () => {
-    intervalId = setInterval(() => {
-        count +=1
-        updateValue()
-    }, 100);
-});
-
-minus_btn.addEventListener('mousedown', () => {
-    intervalId = setInterval(() => {
-        count -= 1
-        updateValue()
-    }, 100);
-});
-
-reset_btn.addEventListener('click', () => {
-    count = 0;
-    updateValue()
+plus.addEventListener('click', () => {
+    updateValue ++;
+    value.innerHTML = updateValue;
 })
 
-document.addEventListener('mouseup', () => clearInterval(intervalId));
+minus.addEventListener('click', () => {
+    updateValue --;
+    value.innerHTML = updateValue;
+})
+
+reset.addEventListener('click', () => {
+    updateValue = 0;
+    value.innerHTML = updateValue;
+})
